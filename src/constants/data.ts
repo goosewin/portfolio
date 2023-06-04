629f8d6bae6ae8023467c4ea1f3d55c0df4a34a8
Daniel Stolbov <daniel@webline.app>
2023-06-03 17:39:30 -0700

    Updated data.ts

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -4,14 +4,14 @@ const userData = {
   designation: "Software Engineer",
   avatarUrl: "/avatar.jpg",
   email: "daniel@webline.app",
-  location: "Sacramento, CA",
+  location: "California",
   projects: [],
   about: {
     title:
-      "Full stack developer with 7+ years of experience building scalable web applications and internal tools that drastically improve efficiency.",
+      "Full-stack software engineer with 7+ years of experience building scalable web applications.",
     description: [
-      "I have worked on a large range of projects and developed strong skills in software engineering by delivering applications for native platforms and web. In the past decade I worked with entrepreneurs and small to medium sized businesses.",
-      "I am Full-Stack Developer — my front-end and back-end expertise come together, essentially covering the full stack of technology for a project. Well versed with Front-End Tools like HTML, CSS, JavaScript; Back-End technologies and infrastructure: NodeJS, Express, NestJS, Fastify.",
+      "A dedicated Full-Stack Software Engineer with extensive experience in various aspects of software development.",
+      "Proven proficiency in a wide range of programming languages and frameworks, including .NET, TypeScript, React.js, AWS, GCP and Agile methodologies. Committed to delivering high-quality, scalable, and efficient software solutions.",
     ],
     currentProject: "",
     currentProjectUrl: "",
